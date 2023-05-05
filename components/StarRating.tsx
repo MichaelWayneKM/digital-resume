f0464e9d3159b61f9a6e01caa1a6e62e719c22c1
Michael Wayne <Kromwell005@gmail.com>
2023-05-05 10:17:04 +0300

    save

--- components/StarRating.tsx
+++ components/StarRating.tsx
@@ -4,59 +4,56 @@ import { faStar as emptyStar } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useSpring, animated } from '@react-spring/web';
 
-
-function FullStar({ i, index}: { i:number, index: number}) {
-
-    const { scale } = useSpring({ from: { scale: 0 }, to: { scale: 1 }, delay: i * index * 100 });
-    
-    
-    return (
-        <animated.div key={i} style={{ transform: scale.interpolate(s => `scale(${s})`) }}>
-        <FontAwesomeIcon icon={solidStar} color="#FFD700" />
-      </animated.div>
-    )
-}
-
-function PartialStar() {
-
-    const { gradient } = useSpring({ from: { gradient: '#A9A9A9' }, to: { gradient: '#FFD700' } });
-
-    return (
-        <animated.div key="partial" style={{ background: gradient.interpolate(g => `linear-gradient(to right, #FFD700 ${g}%, #A9A9A9 ${g}%`) }}>
-        <FontAwesomeIcon icon={solidStar} color="#FFD700" />
-      </animated.div>
-    )
-}
-
 function StarRating({ rating, index }: { rating: number, index: number }) {
   const fullStars = Math.floor(rating);
   const partialStar = rating - fullStars;
   const emptyStars = 5 - fullStars - (partialStar > 0 ? 1 : 0);
-  const starAnimations = [];
-
-  const { scale } = useSpring({ from: { scale: 0 }, to: { scale: 1 }, delay: index * 100 });
-    
   
+  // create animation values
+  const fullStarAnimations = useSpring({
+    from: { scale: 0 },
+    to: { scale: 1 },
+    delay: index * 100,
+    config: { mass: 1, tension: 280, friction: 20 }
+  });
+  
+  const partialStarAnimation = useSpring({
+    from: { gradient: '#A9A9A9' },
+    to: { gradient: '#FFD700' },
+    config: { mass: 1, tension: 280, friction: 20 }
+  });
+  
+  const emptyStarAnimations = useSpring({
+    from: { scale: 0 },
+    to: { scale: 1 },
+    delay: (fullStars * index + 1) * 100,
+    config: { mass: 1, tension: 280, friction: 20 }
+  });
+  
+  const starAnimations = [];
 
   // add colored full star animations
   for (let i = 0; i < fullStars; i++) {
     starAnimations.push(
-      <FullStar i={i} index={index} />
+      <animated.div key={i} style={{ transform: fullStarAnimations.scale.to(s => `scale(${s})`) }}>
+        <FontAwesomeIcon icon={solidStar} color="#FFD700" />
+      </animated.div>
     );
   }
 
   // add partial star animation
   if (partialStar > 0) {
-
     starAnimations.push(
-      <PartialStar />
+      <animated.div key="partial" style={{ background: partialStarAnimation.gradient.to(g => `linear-gradient(to right, #FFD700 ${g}%, #A9A9A9 ${g}%`) }}>
+        <FontAwesomeIcon icon={solidStar} color="#FFD700" />
+      </animated.div>
     );
   }
 
   // add empty star animations
   for (let i = 0; i < emptyStars; i++) {
     starAnimations.push(
-      <animated.div key={`empty-${i}`} style={{ transform: scale.interpolate(s => `scale(${s})`) }}>
+      <animated.div key={`empty-${i}`} style={{ transform: emptyStarAnimations.scale.to(s => `scale(${s})`) }}>
         <FontAwesomeIcon icon={emptyStar} color="#A9A9A9" />
       </animated.div>
     );
